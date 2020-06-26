9cded82f0a390073969fcf1eaaa1e0532722a3c8
xenoveals <xenoveals@gmail.com>
2020-06-26 23:23:19 +0700

    sign up sign in (+js)

--- frontend-only/js/register.js
+++ frontend-only/js/register.js
@@ -4,38 +4,20 @@ $('#registerEmail').on('input', function(){
 	$('#registerEmail').removeClass('is-invalid');
 	if(($('#registerEmail').val().length > 7) && (is_email)){
 		$('#registerEmail').removeClass('is-invalid').addClass('is-valid')
-		$('#msg2').html('');
+		$('#msg').html('');
 	}else{
 		$('#registerEmail').removeClass('is-valid').addClass('is-invalid');
-		$('#msg2').html('Give a valid email!');
+		$('#msg').html('Give a valid email!');
 	}
-});
-
-$('#loginEmail').on('input', function(){
-	var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-	var is_email=re.test($('#loginEmail').val());
-
-	if(($('#loginEmail').val().length > 7) && (is_email)){
-		$('#loginEmail').removeClass('is-invalid').addClass('is-valid')
-		$('#msg2').html('');
-	}else{
-		$('#loginEmail').removeClass('is-valid').addClass('is-invalid');
-		$('#msg2').html('Give a valid email!');
-	}
-});
-
-$('#loginPass').on('input', function(){
-	$('#loginPass').removeClass('is-invalid')
-	$('#msg').html('');
 });
 
 $('#registerPass').on('input', function(){
 	if(!(($('#registerPass').val().length > 7) && (/\d/.test($('#registerPass').val())))){
 		$('#registerPass').removeClass('is-valid').addClass('is-invalid');
-		$('#msg').html('Password minimum 8 characters with numeric values');
+		$('#msg2').html('Password minimum 8 characters with numeric values');
 	}
 	else{
-		$('#msg').html('');
+		$('#msg2').html('');
 		$('#registerPass').removeClass('is-invalid').addClass('is-valid')
 	}
 });
@@ -43,24 +25,24 @@ $('#registerPass').on('input', function(){
 $('#registerPass2').on('input', function(){
 	if($('#registerPass').val() != $('#registerPass2').val()){
 		$('#registerPass2').removeClass('is-valid').addClass('is-invalid');
-		$('#msg').html('Password didn\'t Match!');
+		$('#msg2').html('Password didn\'t Match!');
 	}else{
-		$('#msg').html('');
+		$('#msg2').html('');
 		$('#registerPass2').removeClass('is-invalid').addClass('is-valid');
 	}
 });
 
-$("#submit-register").click(function(event){
-	var form_data=$("#register").serializeArray();
+$("#sign-up").click(function(event){
+	var form_data=["#registerEmail", "#registerPass", "registerPass2"];
 	var error_free=true;
 	for (var index in form_data){
-		var element=$("#"+form_data[index]['name']);
+		var element=$(form_data[index]);
 		var isnotvalid=element.hasClass("is-invalid");
 		var element_value=element.val()
 		if (isnotvalid || !element_value){error_free=false;}
 	}
-	var agree_check=$("#agreePolicy").is(":checked")
-	if(!agree_check){error_free=false}
+	//var agree_check=$("#agreePolicy").is(":checked")
+	//if(!agree_check){error_free=false}
 	if (!error_free){
 		event.preventDefault(); 
 	}
