855339ff8ea3948c237cb2b55925924399543275
xenoveals <xenoveals@gmail.com>
2020-07-03 19:50:20 +0700

    frontend timeline fixed

--- frontend-only/js/timeline.js
+++ frontend-only/js/timeline.js
@@ -2,16 +2,20 @@ function pin(theid){
     var x = $("#"+theid)
     if(x.hasClass("active2")){
         x.removeClass("active2")
+        x.html(`<span class="iconify" data-icon="bi:bookmark" data-inline="false"></span>`)
     }else{
         x.addClass("active2")
+        x.html(`<span class="iconify" data-icon="bi:bookmark-fill" data-inline="false"></span>`)
     }
 }
 function like(theid){
     var x = $("#"+theid)
     if(x.hasClass("active")){
         x.removeClass("active")
+        x.html(`<span class="iconify" data-icon="clarity:heart-line" data-inline="false"></span>`)
     }else{
         x.addClass("active")
+        x.html(`<span class="iconify" data-icon="clarity:heart-solid" data-inline="false"></span>`)
     }
 }
 function comment(theid){
@@ -24,6 +28,14 @@ function comment(theid){
 }
 $(".top .profile img").click(function(){
     var x = $(".profile-expand")
+    if(x.hasClass("hide")){
+        x.removeClass("hide")
+    }else{
+        x.addClass("hide")
+    }
+})
+$("#filter").click(function(){
+    var x = $("#filter-expand")
     if(x.hasClass("hide")){
         x.removeClass("hide")
     }else{
