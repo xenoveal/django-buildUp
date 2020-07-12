20e817551b05e8dddb2381adacd6682744004bfa
xenoveals <xenoveals@gmail.com>
2020-07-13 00:09:48 +0700

    blog

--- frontend-only/js/collaboration.js
+++ frontend-only/js/collaboration.js
@@ -10,3 +10,22 @@ function hide(){
     document.getElementById('new').setAttribute("class", "register-new");
 }
 
+function comment(theid){
+    var x = $("#"+theid)
+    if(x.hasClass("hide")){
+        x.removeClass("hide")
+    }else{
+        x.addClass("hide")
+    }
+}
+
+function pin(theid){
+    var x = $("#"+theid)
+    if(x.hasClass("active2")){
+        x.removeClass("active2")
+        x.html(`<span class="iconify" data-icon="bi:bookmark" data-inline="false"></span>`)
+    }else{
+        x.addClass("active2")
+        x.html(`<span class="iconify" data-icon="bi:bookmark-fill" data-inline="false"></span>`)
+    }
+}
